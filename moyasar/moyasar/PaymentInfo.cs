f5c300ec85ce8aa1971a7f87ff72aa72c3894211
samer abdulla <smsssamer@live.com>
2016-11-21 17:04:23 +0300

    add Info of Payment

---
+++ moyasar/moyasar/PaymentInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moyasar
+{
+   public class PaymentInfo
+    {
+       public string type { get; set; }
+       public string Company { get; set; }
+       public string Name { get; set; }
+       public string Number { get; set; }
+       public string Message { get; set; }
+    }
+}
---
+++ moyasar/moyasar/PaymentResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moyasar
+{
+   public class PaymentResult
+    {
+       public string Id { get; set; }
+       public string Status { get; set; }
+       public double   Amount { get; set; }
+       public double Fee { get; set; }
+       public string Currency { get; set; }
+       public double Refunded { get; set; }
+       public string Refunded_at { get; set; }
+       public string Description { get; set; }
+       public string Invoice_Id { get; set; }
+       public string IP { get; set; }
+       public string Created_at { get; set; }
+       public string Updated_at { get; set; }
+
+    }
+}
